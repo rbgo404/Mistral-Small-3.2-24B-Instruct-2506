93a6607c14698a0d570255177841e40b37507f4c
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2025-03-28 10:11:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,13 @@ class InferlessPythonModel:
         )
 
     def infer(self, request: RequestObjects) -> ResponseObjects:
+        system_prompt = "You are a conversational agent that always answers straight to the point."
+        if request.system_prompt is not None:
+            system_prompt = request.system_prompt
         messages = [
                         {
                             "role": "system",
-                            "content": request.system_prompt
+                            "content": system_prompt
                         },
                         {
                             "role": "user",
