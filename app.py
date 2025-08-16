36417dfa402819a3ec0ed1029add307c6cb2429c
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-08-16 13:41:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import os
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
-from huggingface_hub import snapshot_download
 from pydantic import BaseModel, Field
 from typing import Optional
 import inferless
+
+from datetime import datetime, timedelta
 import torch
-from transformers import AutoTokenizer, Mistral3ForConditionalGeneration, BitsAndBytesConfig
+from mistral_common.protocol.instruct.request import ChatCompletionRequest
+from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+from huggingface_hub import hf_hub_download
+from transformers import Mistral3ForConditionalGeneration
+
+
 
 
 @inferless.request
 class RequestObjects(BaseModel):
     prompt: str = Field(default="Give me 5 non-formal ways to say 'See you later' in French.")
+    image_url: Optional[str] = "https://static.wikia.nocookie.net/essentialsdocs/images/7/70/Battle.png/revision/latest?cb=20220523172438"
     system_prompt: str = Field(default="You are a conversational agent that always answers straight to the point.")
     temperature: float = Field(default=0.7)
     top_p: float = Field(default=0.1)
@@ -24,46 +31,56 @@ class ResponseObjects(BaseModel):
     generated_text: str = Field(default="Test output")
 
 class InferlessPythonModel:
+    def load_system_prompt(self,repo_id: str, filename: str) -> str:
+        file_path = hf_hub_download(repo_id=repo_id, filename=filename)
+        with open(file_path, "r") as file:
+            system_prompt = file.read()
+        today = datetime.today().strftime("%Y-%m-%d")
+        yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+        model_name = repo_id.split("/")[-1]
+        return system_prompt.format(name=model_name, today=today, yesterday=yesterday)
+
     def initialize(self):
         model_id = "mistralai/Mistral-Small-3.2-24B-Instruct-2506"
-        snapshot_download(repo_id=model_id, allow_patterns=["*.safetensors"])
-        quantization_config = BitsAndBytesConfig(load_in_4bit=True)
-        
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-        self.tokenizer.chat_template = "{%- set today = strftime_now(\"%Y-%m-%d\") %}\n{%- set default_system_message = \"You are Mistral Small 3, a Large Language Model (LLM) created by Mistral AI, a French startup headquartered in Paris.\\nYour knowledge base was last updated on 2023-10-01. The current date is \" + today + \".\\n\\nWhen you're not sure about some information, you say that you don't have the information and don't make up anything.\\nIf the user's question is not clear, ambiguous, or does not provide enough context for you to accurately answer the question, you do not try to answer it right away and you rather ask the user to clarify their request (e.g. \\\"What are some good restaurants around me?\\\" => \\\"Where are you?\\\" or \\\"When is the next flight to Tokyo\\\" => \\\"Where do you travel from?\\\")\" %}\n\n{{- bos_token }}\n\n{%- if messages[0]['role'] == 'system' %}\n    {%- set system_message = messages[0]['content'] %}\n    {%- set loop_messages = messages[1:] %}\n{%- else %}\n    {%- set system_message = default_system_message %}\n    {%- set loop_messages = messages %}\n{%- endif %}\n{{- '[SYSTEM_PROMPT]' + system_message + '[/SYSTEM_PROMPT]' }}\n\n{%- for message in loop_messages %}\n    {%- if message['role'] == 'user' %}\n\t    {%- if message['content'] is string %}\n            {{- '[INST]' + message['content'] + '[/INST]' }}\n\t    {%- else %}\n\t\t    {{- '[INST]' }}\n\t\t    {%- for block in message['content'] %}\n\t\t\t    {%- if block['type'] == 'text' %}\n\t\t\t\t    {{- block['text'] }}\n\t\t\t    {%- elif block['type'] == 'image' or block['type'] == 'image_url' %}\n\t\t\t\t    {{- '[IMG]' }}\n\t\t\t\t{%- else %}\n\t\t\t\t    {{- raise_exception('Only text and image blocks are supported in message content!') }}\n\t\t\t\t{%- endif %}\n\t\t\t{%- endfor %}\n\t\t    {{- '[/INST]' }}\n\t\t{%- endif %}\n    {%- elif message['role'] == 'system' %}\n        {{- '[SYSTEM_PROMPT]' + message['content'] + '[/SYSTEM_PROMPT]' }}\n    {%- elif message['role'] == 'assistant' %}\n        {{- message['content'] + eos_token }}\n    {%- else %}\n        {{- raise_exception('Only user, system and assistant roles are supported!') }}\n    {%- endif %}\n{%- endfor %}"
-        
-        self.model = Mistral3ForConditionalGeneration.from_pretrained(
-            model_id,
-            trust_remote_code=True,
-            quantization_config=quantization_config,
-            device_map="cuda",
-        )
+        self.SYSTEM_PROMPT = self.load_system_prompt(model_id, "SYSTEM_PROMPT.txt")       
+        self.tokenizer = MistralTokenizer.from_hf_hub(model_id)
+        self.model = Mistral3ForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.bfloat16)
 
-    def infer(self, request: RequestObjects) -> ResponseObjects:
+    def infer(self, inputs: RequestObjects) -> ResponseObjects:       
+        user_message = [
+            {
+                "type": "text",
+                "text": inputs.prompt,
+            }]
+        if inputs.image_url:
+            print("inputs.image_url: ",inputs.image_url,flush=True)
+            user_message.append({"type": "image_url", "image_url": {"url": inputs.image_url}})
+        
         messages = [
-                        {
-                            "role": "system",
-                            "content": request.system_prompt
-                        },
-                        {
-                            "role": "user",
-                            "content": request.prompt
-                        },
-                    ]
-        tokenized_chat = self.tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt").to("cuda")
-        with torch.no_grad():
-            generation = self.model.generate(
-                tokenized_chat,
-                max_new_tokens=request.max_tokens,
-                temperature=request.temperature,
-                top_p=request.top_p,
-                top_k=request.top_k,
-                do_sample=request.do_sample,
-                repetition_penalty=request.repetition_penalty,
-            )
-            generated_text = self.tokenizer.decode(generation[0], skip_special_tokens=True)
+            {"role": "system", "content": self.SYSTEM_PROMPT},
+            {
+                "role": "user",
+                "content": user_message,
+            },
+        ]
+        tokenized = self.tokenizer.encode_chat_completion(ChatCompletionRequest(messages=messages))
+        
+        input_ids = torch.tensor([tokenized.tokens])
+        attention_mask = torch.ones_like(input_ids)
+        pixel_values = torch.tensor(tokenized.images[0], dtype=torch.bfloat16).unsqueeze(0)
+        image_sizes = torch.tensor([pixel_values.shape[-2:]])
+        
+        output = self.model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            pixel_values=pixel_values,
+            image_sizes=image_sizes,
+            max_new_tokens=1000,
+        )[0]
+        
+        decoded_output = self.tokenizer.decode(output[len(tokenized.tokens) :])
 
-        return ResponseObjects(generated_text=generated_text)
+        return ResponseObjects(generated_text=decoded_output)
 
     def finalize(self):
         self.model = None
